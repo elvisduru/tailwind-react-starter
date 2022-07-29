8ca98327e33ab2603ef2a0edca09a26a128aa982
Elvis Duru <duruelvis95@gmail.com>
2022-07-29 02:44:00 +0100

    Initialize Repository

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="max-w-4xl mx-auto text-center h-screen flex flex-col justify-center">
+      <h1 className="text-6xl">Hello Tailwind</h1>
+    </div>
+  );
+}
+
+export default App;
